02706d02a78ddf072d1cfa259718a4c998de556a
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-06-15 13:44:04 -0300

    fim do mobile first aparentemente

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ function abrirFormulario() {
     window.document.getElementById('button-mais').hidden = true;
     window.document.getElementById('formu').hidden = false;
 }
+
+
+function cancelar() {
+    
+    window.document.getElementById('formu').hidden = true;
+    window.document.getElementById('button-mais').hidden = false;
+    window.document.getElementById('txttarefa').value = '';
+    window.document.getElementById('txtdata').value = '';
+    window.document.getElementById('txthorario').value = '';
+}
