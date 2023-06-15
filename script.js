31fa422724d428edff28723428facba2d5adaecc
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-06-15 13:29:37 -0300

    Botões de cancelar e confirmar

---
+++ script.js
@@ -0,0 +1,5 @@
+function abrirFormulario() {
+    window.document.getElementById('textoInicial').hidden = true;
+    window.document.getElementById('button-mais').hidden = true;
+    window.document.getElementById('formu').hidden = false;
+}
